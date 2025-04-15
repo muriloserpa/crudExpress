823c6fd2cdc297c5884036797a411aa42b77d4af
Murilo Serpa <murilo.serpa11@gmail.com>
2025-04-15 18:42:22 -0300

    init express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import Express from "express";
+
+const app = Express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
